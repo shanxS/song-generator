e0b0b42d736efd8d08e3cabd979672e644b46a66
shashank kumar <shanx.shashank@gmail.com>
2014-11-07 20:53:36 +0530

    adding files which should ahve been added in previous commit

---
+++ getFieldName.m
@@ -0,0 +1,3 @@
+function name = getFieldName()
+    name = "spectrum";
+end
---
+++ getZeroOutSignal.m
@@ -0,0 +1,12 @@
+function newSignal = getZeroOutSignal(timeBinData)
+    numTimeBin = length(timeBinData);
+    
+    signal = timeBinData(1, 1);
+    numFrequencyBin = length(signal.(getFieldName()));
+    
+    for timeBinNumber = 1:numTimeBin
+        newSignal(timeBinNumber).(getFieldName()) = zeros \
+                                    (1, numFrequencyBin);
+    end
+    
+end
